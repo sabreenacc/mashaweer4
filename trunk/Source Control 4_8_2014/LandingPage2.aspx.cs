00776dfcfbde3ed89f6ad9ff7822ce9e7dcdc423
sabreen <sabreen>
2014-12-08 06:49:16 +0000

    git-svn-id: https://premitek.xp-dev.com/svn/mashaweer@3 ab907a69-6a5b-4c5d-be20-cad7d5b76572

--- trunk/Source Control 4_8_2014/LandingPage2.aspx.cs
+++ trunk/Source Control 4_8_2014/LandingPage2.aspx.cs
@@ -5,11 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page
+public partial class LandingPage2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-       // ltrlCalculatorHome.Text = Resources.LanguageFile.CALCULATOR_HOMEPAGE;
 
     }
 }
